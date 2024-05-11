50ebc4848cbe1dbc7110bdcc8c9527271f4d45d5
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2024-05-11 14:45:06 -0400

    Move fonts.tsx into global dir

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,10 @@
  * https://nextjs.org/docs/pages/building-your-application/routing/custom-app
  */
 
-import "@/styles/globals.css";
+import "@/global/style.css";
 import type { AppProps } from "next/app";
 
-import { inter, playfair_display } from "@/fonts";
+import { inter, playfair_display } from "@/global/fonts";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
