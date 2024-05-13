c310c16dfe3fed7858808c15a34579e1a6940274
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2024-05-13 14:58:10 -0400

    Move head from root index to _document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,9 @@
-import Head from "next/head";
 import Hero from "@/components/Hero/Hero";
 import Artwork from "@/components/Artwork/Artwork";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Tabitha Bernard - Artist Portfolio</title>
-        <meta name="description" content="Artist Portfolio - Tabitha Bernard" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Hero />
       <Artwork />
     </>
