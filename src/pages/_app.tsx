2ee8971806cd289517e7a8bb1f415a32d0cb18e9
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2024-05-14 13:19:29 -0400

    Move custom Head to _app (err no-document-title)
    https://nextjs.org/docs/messages/no-document-title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@
 
 import "@/global/style.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import { inter, playfair_display } from "@/global/fonts";
 
@@ -14,6 +15,12 @@ import Footer from "@/components/Footer/Footer";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Tabitha Bernard - Artist Portfolio</title>
+        <meta name="description" content="Artist Portfolio - Tabitha Bernard" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <style jsx global>{`
         :root {
           --inter-font: ${inter.style.fontFamily};
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,12 +8,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Tabitha Bernard - Artist Portfolio</title>
-        <meta name="description" content="Artist Portfolio - Tabitha Bernard" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
