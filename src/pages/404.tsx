792fef0f69d7740811a675e3725ac1973ce7a337
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2024-05-16 15:41:50 -0400

    Create 404.tsx

---
+++ src/pages/404.tsx
@@ -0,0 +1,4 @@
+// https://nextjs.org/docs/pages/building-your-application/routing/custom-error#404-page
+export default function Custom404() {
+  return <h1 style={{ textAlign: "center" }}>404 - Page Not Found</h1>;
+}
