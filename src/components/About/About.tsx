200c89ec68ef287608b71658ef746ebf06f50e16
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2024-05-19 14:47:16 -0400

    Add about page & component

---
+++ src/components/About/About.tsx
@@ -0,0 +1,33 @@
+import styles from "./About.module.css";
+
+export default function About() {
+  return (
+    <main className={styles.main}>
+      <h1>About Tabitha</h1>
+      <p>
+        With a rich background spanning over 15 years in IT project management
+        within the banking sector, Tabitha made the bold decision to follow her
+        true passion for colour and design. She left the structured world of
+        technology, to embrace a journey of creativity and self-expression.
+      </p>
+      <p>
+        Enrolling in an interior decorating course at George Brown, Tabitha
+        mastered the principles of design and colour, which laid the foundation
+        for her various creative pursuits. Starting with Interior Design, she
+        specialized in kitchen and bathroom renovations where she combined
+        aesthetic flair with meticulous project management to create functional
+        but beautiful spaces that delighted her clients.
+      </p>
+      <p>
+        In her spare time she continued to hone her sewing and garment making
+        skills and discovered her passion for cake decorating. In recent years,
+        Tabitha has been focused on mastering the art of painting, using her
+        understanding of design and colour to bring scenes to life on canvas.
+        For Tabitha, creativity knows no bounds, whether it's orchestrating the
+        perfect room transformation, sewing a new garment, painting vibrant
+        landscapes, or crafting delectable confections, she finds fulfillment in
+        the boundless realms of creativity.
+      </p>
+    </main>
+  );
+}
