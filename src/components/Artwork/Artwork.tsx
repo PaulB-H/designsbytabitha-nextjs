26656d884dfbad97b7fd801ae39e8ada2e671dfc
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2024-05-24 16:23:20 -0400

    Add more paintings to artwork section

--- src/components/Artwork/Artwork.tsx
+++ src/components/Artwork/Artwork.tsx
@@ -90,6 +90,136 @@ export default function Artwork() {
               <p>18” X 24”</p>
             </div>
           </div>
+
+          <div className={styles.grid_item}>
+            <FullScreenImage
+              src="/artwork/paintings/Aurora_Borealis_diptych_8X10_1920x734.webp"
+              alt="Image"
+              classList={clsx(styles.painting)}
+            />
+            <div className={styles.textBox}>
+              <p className={styles.bold}>Aurora Borealis</p>
+              <p>Acrylic on Canvas</p>
+              <p>8” X 10”</p>
+            </div>
+          </div>
+
+          <div className={styles.grid_item}>
+            <FullScreenImage
+              src="/artwork/paintings/Celestial_14X18_1400x1080.webp"
+              alt="Image"
+              classList={clsx(styles.painting)}
+            />
+            <div className={styles.textBox}>
+              <p className={styles.bold}>Celestial</p>
+              <p>Acrylic on Canvas</p>
+              <p>14” X 18”</p>
+            </div>
+          </div>
+
+          <div className={styles.grid_item}>
+            <FullScreenImage
+              src="/artwork/paintings/emotion_1_8X10_1378x1080.webp"
+              alt="Image"
+              classList={clsx(styles.painting)}
+            />
+            <div className={styles.textBox}>
+              <p className={styles.bold}>Emotion 1</p>
+              <p>Acrylic on Canvas</p>
+              <p>8” X 10”</p>
+            </div>
+          </div>
+
+          <div className={styles.grid_item}>
+            <FullScreenImage
+              src="/artwork/paintings/emotion_2_8X10_1360x1080.webp"
+              alt="Image"
+              classList={clsx(styles.painting)}
+            />
+            <div className={styles.textBox}>
+              <p className={styles.bold}>Emotion 2</p>
+              <p>Acrylic on Canvas</p>
+              <p>8” X 10”</p>
+            </div>
+          </div>
+
+          <div className={styles.grid_item}>
+            <FullScreenImage
+              src="/artwork/paintings/life_diptych_8X10_1853x1080.webp"
+              alt="Image"
+              classList={clsx(styles.painting)}
+            />
+            <div className={styles.textBox}>
+              <p className={styles.bold}>Life</p>
+              <p>Acrylic on Canvas</p>
+              <p>8” X 10”</p>
+            </div>
+          </div>
+
+          <div className={styles.grid_item}>
+            <FullScreenImage
+              src="/artwork/paintings/Line_and_texture_Diptych_12X16_1920x673.webp"
+              alt="Image"
+              classList={clsx(styles.painting)}
+            />
+            <div className={styles.textBox}>
+              <p className={styles.bold}>Line & Texture</p>
+              <p>Acrylic on Canvas</p>
+              <p>12” X 16”</p>
+            </div>
+          </div>
+
+          <div className={styles.grid_item}>
+            <FullScreenImage
+              src="/artwork/paintings/Muted_Flowers_9X12_1435x1080.webp"
+              alt="Image"
+              classList={clsx(styles.painting)}
+            />
+            <div className={styles.textBox}>
+              <p className={styles.bold}>Muted Flowers</p>
+              <p>Acrylic on Canvas</p>
+              <p>9” X 12”</p>
+            </div>
+          </div>
+
+          <div className={styles.grid_item}>
+            <FullScreenImage
+              src="/artwork/paintings/Ocean_Blue_12X16_1440x1080.webp"
+              alt="Image"
+              classList={clsx(styles.painting)}
+            />
+            <div className={styles.textBox}>
+              <p className={styles.bold}>Ocean Blue</p>
+              <p>Acrylic on Canvas</p>
+              <p>12” X 16”</p>
+            </div>
+          </div>
+
+          <div className={styles.grid_item}>
+            <FullScreenImage
+              src="/artwork/paintings/Ocean_Blue_small_9X12_819x1080.webp"
+              alt="Image"
+              classList={clsx(styles.painting)}
+            />
+            <div className={styles.textBox}>
+              <p className={styles.bold}>Ocean Blue Small</p>
+              <p>Acrylic on Canvas</p>
+              <p>9” X 12”</p>
+            </div>
+          </div>
+
+          <div className={styles.grid_item}>
+            <FullScreenImage
+              src="/artwork/paintings/Turmoil_12X16_1439x1080.webp"
+              alt="Image"
+              classList={clsx(styles.painting)}
+            />
+            <div className={styles.textBox}>
+              <p className={styles.bold}>Turmoil</p>
+              <p>Acrylic on Canvas</p>
+              <p>12” X 16”</p>
+            </div>
+          </div>
         </div>
       </main>
     </div>
